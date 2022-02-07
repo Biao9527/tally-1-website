542497368ac9b9c4ef19feacdb5ab23699df0bb3
周字彪 <1247821135@qq.com>
2022-02-07 14:23:49 +0800

    deploy

---
+++ precache-manifest.0f421ae96259de52fd3569f0b883eadc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a6e21fd59d0d5193215",
+    "url": "/tally-1-website/css/app.0bea7293.css"
+  },
+  {
+    "revision": "42389928bdf64a3c9fd14a7bf94b9edc",
+    "url": "/tally-1-website/index.html"
+  },
+  {
+    "revision": "1a6e21fd59d0d5193215",
+    "url": "/tally-1-website/js/app.482a9bb6.js"
+  },
+  {
+    "revision": "92d3f79cf200a52d428f",
+    "url": "/tally-1-website/js/chunk-vendors.010eeb8b.js"
+  },
+  {
+    "revision": "3b3ac03b730001df064d3e92befbe4e5",
+    "url": "/tally-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tally-1-website/robots.txt"
+  }
+]);
